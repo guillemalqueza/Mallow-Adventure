00eefe182a713e24b622aa7d93b95ff75651cd65
Guillem Alqueza Mayench <guillem.alqueza@estudiantat.upc.edu>
2023-10-30 13:55:03 +0100

    F9

--- PlatformerGame/Game/Source/App.h
+++ PlatformerGame/Game/Source/App.h
@@ -85,6 +85,8 @@ public:
 	Map* map;
 	Physics* physics;
 
+	uint32 maxFrameDuration = 16;
+
 private:
 
 	int argc;
@@ -113,8 +115,6 @@ private:
 
 	float averageFps = 0.0f;
 	uint32 secondsSinceStartup = 0;
-
-	uint32 maxFrameDuration = 16;
 
 };
 
