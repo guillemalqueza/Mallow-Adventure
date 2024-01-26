b02d0b9df2f058d26c087b05a28630dd542ec425
guillemal <guillem.alqueza@estudiantat.upc.edu>
2024-01-26 11:57:00 +0100

    chest

--- PlatformerGame/Game/Source/Scene.cpp
+++ PlatformerGame/Game/Source/Scene.cpp
@@ -46,7 +46,6 @@ bool Scene::Awake(pugi::xml_node& config)
 	CreateEntities(config, "skeleton", EntityType::SKELETON);
 	CreateEntities(config, "ghost", EntityType::GHOST);
 	CreateEntities(config, "obstacle", EntityType::OBSTACLE);
-	CreateEntities(config, "chest", EntityType::CHEST);
 	CreateEntities(config, "log", EntityType::LOG_OBSTACLE);
 
 	app->entityManager->GetEnemies(skeletonsList, ghostsList);
