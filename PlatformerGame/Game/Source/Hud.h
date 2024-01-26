ab18085d99652b1f763520eb5c5fc9f1dc45ed38
AlejandroIR <shibainuale@gmail.com>
2024-01-26 13:42:47 +0100

    pause buttons + win

--- PlatformerGame/Game/Source/Hud.h
+++ PlatformerGame/Game/Source/Hud.h
@@ -38,6 +38,35 @@ private:
 
 	SDL_Rect lifeRect = { 0,0,32,32 };
 
+
+	SDL_Texture* exitNormal;
+	SDL_Texture* exitHover;
+	SDL_Texture* exitClick;
+
+	SDL_Texture* returnNormal;
+	SDL_Texture* returnHover;
+	SDL_Texture* returnClick;
+
+	SDL_Texture* pause;
+
+	SDL_Texture* resumeNormal;
+	SDL_Texture* resumeHover;
+	SDL_Texture* resumeClick;
+
+	SDL_Texture* settingsNormal;
+	SDL_Texture* settingsHover;
+	SDL_Texture* settingsClick;
+
+	SDL_Texture* backToTitleNormal;
+	SDL_Texture* backToTitleHover;
+	SDL_Texture* backToTitleClick;
+
+
+	GuiControlButton* exitButton;
+	GuiControlButton* returnButton;
+	GuiControlButton* resumeButton;
+	GuiControlButton* settingsButton;
+	GuiControlButton* backToTitleButton;
 };
 
 #endif // __HUD_H__
